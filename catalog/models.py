ff0b6b8e451a6b6bc41b9a04ec021020accafbcf
Anna Novikova <anna.novikova.dev@gmail.com>
2025-09-01 10:23:52 +0300

    model created and registered

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование категории')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['name']
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Имя')
+    description = models.TextField(verbose_name='Описание')
+    photo = models.ImageField(upload_to='catalog/photo', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, max_length=150, verbose_name='Категория', related_name='products')
+    price = models.FloatField(verbose_name='цена за покупку')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ['category', 'name']
